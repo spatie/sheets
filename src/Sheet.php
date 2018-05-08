70264c3a1edfb41c1600d3cb71a1b2bed4694063
Freek <freek@spatie.be>
2018-05-08 15:29:52 +0200

    improve readability

--- src/Sheet.php
+++ src/Sheet.php
@@ -16,8 +16,10 @@ class Sheet
     {
         $value = $this->attributes[$key] ?? null;
 
-        if (method_exists($this, 'get'.studly_case($key).'Attribute')) {
-            return $this->{'get'.studly_case($key).'Attribute'}($value);
+        $accessorFunctionName = 'get'.studly_case($key).'Attribute';
+
+        if (method_exists($this, $accessorFunctionName)) {
+            return $this->$accessorFunctionName($value);
         }
 
         return $value;
