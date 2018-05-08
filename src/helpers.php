65a789f949241b77661554179b8ae240b431cd48
Freek <freek@spatie.be>
2018-05-08 15:26:45 +0200

    remove useless comment

--- src/helpers.php
+++ src/helpers.php
@@ -3,9 +3,6 @@
 use Spatie\Sheets\Sheets;
 
 if (! function_exists('sheets')) {
-    /**
-     * @return \Spatie\Sheets\Sheets
-     */
     function sheets(): Sheets
     {
         return app(Sheets::class);
