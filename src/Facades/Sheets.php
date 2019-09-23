27ce28c45a05960746a127fe0c9db457a0154d6c
Tom Witkowski <dev.gummibeer@gmail.com>
2019-09-23 17:44:06 +0200

    add doc-tag for facade

--- src/Facades/Sheets.php
+++ src/Facades/Sheets.php
@@ -4,7 +4,16 @@ namespace Spatie\Sheets\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/** @see \Spatie\Sheets\Sheets */
+/**
+ * @method static \Spatie\Sheets\Repository collection(string $name)
+ * @method static void registerCollection(string $name, \Spatie\Sheets\Repository $repository)
+ * @method static ?\Spatie\Sheets\Sheet get(string $path)
+ * @method static \Illuminate\Support\Collection all()
+ * @method static void setDefaultCollection(string $defaultCollection)
+ * @method static \Illuminate\Support\Collection getRegisteredCollectionNames()
+ *
+ * @see \Spatie\Sheets\Sheets
+ */
 class Sheets extends Facade
 {
     protected static function getFacadeAccessor()
