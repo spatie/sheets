b06d2930053d0e2ff20db05ec9c26ff378b8743d
Tom Witkowski <dev.gummibeer@gmail.com>
2019-10-07 08:57:56 +0200

    add yaml and json parser (#41)

---
+++ tests/ContentParsers/YamlParserTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Spatie\Sheets\Tests\ContentParsers;
+
+use PHPUnit\Framework\TestCase;
+use Spatie\Sheets\ContentParsers\MarkdownParser;
+use League\CommonMark\CommonMarkConverter;
+use Spatie\Sheets\ContentParsers\YamlParser;
+
+class YamlParserTest extends TestCase
+{
+    /** @test */
+    public function it_converts_a_front_matter_document_to_attributes()
+    {
+        $yamlParser = new YamlParser();
+
+        $contents = implode("\n", [
+            'title: Hello, world!',
+            'attribute:',
+            '  data:',
+            '    - foo',
+            '    - bar',
+        ]);
+
+        $expected = [
+            'title' => 'Hello, world!',
+            'attribute' => [
+                'data' => [
+                    'foo',
+                    'bar',
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expected, $yamlParser->parse($contents));
+    }
+}
