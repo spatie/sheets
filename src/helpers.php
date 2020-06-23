fe20532abd7b8a5797034bea2e17b15f24109fae
Tom Witkowski <dev.gummibeer@gmail.com>
2020-06-24 00:35:21 +0200

    allow to directly pass the collection name into sheets() helper function

--- src/helpers.php
+++ src/helpers.php
@@ -3,8 +3,17 @@
 use Spatie\Sheets\Sheets;
 
 if (! function_exists('sheets')) {
-    function sheets(): Sheets
+    /**
+     * @param string|null $collection
+     *
+     * @return \Spatie\Sheets\Repository|\Spatie\Sheets\Sheets
+     */
+    function sheets(?string $collection = null)
     {
-        return app(Sheets::class);
+        if ($collection === null) {
+            return app(Sheets::class);
+        }
+
+        return app(Sheets::class)->collection($collection);
     }
 }
